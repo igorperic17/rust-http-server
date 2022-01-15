e554b4b3d62a339d32bafd9794400b162603fa57
Igor Peric <igorperic@live.com>
2022-01-15 12:39:22 +0100

    First connection accepted

---
+++ server/src/http/method.rs
@@ -0,0 +1,11 @@
+pub enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTION,
+    TRACE,
+    PATCH,
+}
---
+++ server/src/server.rs
@@ -0,0 +1,34 @@
+use log::error;
+use std::io::Read;
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Server { addr: addr }
+    }
+
+    pub fn run(&self) {
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        println!("Listening on {}", self.addr);
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
+    }
+}
